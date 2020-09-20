d8495966939f8e247242e70004bde985c6df10e0
Pauline Telan <pauline.telan@gmail.com>
2020-09-20 15:24:32 -0600

    added active share button state

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 
 $(".share-button")
-  .mouseup(function (event) {
-    console.log(event);
-    $(".share-button").toggleClass("pressed");
-    $(".share-popup")
-      .toggleClass("show")
-      .css({
-      // left: Math.min(event.offsetX, $(window).innerWidth()-$('.share-button').outerWidth()),
-      // top: event.offsetY + $('.share-button').innerHeight()
-    });
-  })
-  .mousedown(function () {
-    $(".share-button").toggleClass("pressed");
-});
+  .click(function (event) {
+    $(".share-button").toggleClass("active");
+    $(".share-svg").toggleClass("active");
+    $(".share-popup").toggleClass("show");
+  });
